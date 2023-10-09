50e44ccecd093888602951ee1336767045fb11fd
Fabien Boucher <fboucher@redhat.com>
2023-10-09 08:49:38 +0000

    flake bump

--- flake.nix
+++ flake.nix
@@ -1,19 +1,19 @@
 {
   description = "Monocle Operator Project";
   nixConfig.bash-prompt = "[nix(monocle-operator)] ";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/22.11"; };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/23.05"; };
 
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
     in {
       devShells.x86_64-linux.default = pkgs.mkShell {
-        name = "My-project build environment";
+        name = "Monocle-Operator build environment";
         buildInputs = [
-          # 1.25.4 in nixpkgs 22.11
+          # 1.27.1 in nixpkgs
           pkgs.kubectl
-          # 1.19.3 in nixpkgs 22.11
+          # 1.20.4 in nixpkgs
           pkgs.go
-          # 0.10.1 in nixpkgs 22.11
+          # 0.11.0 in nixpkgs
           pkgs.gopls
           pkgs.k9s
         ];
